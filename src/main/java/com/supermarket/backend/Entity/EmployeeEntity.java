a8fdd665d9eeda2a1c484bc8b4960ef8f3248ed0
Trần Minh Quang <tmquang0209@gmail.com>
2024-06-30 18:55:26 +0700

    chore: Add email field to EmployeeEntity constructor and SignupRequest constructor

--- src/main/java/com/supermarket/backend/Entity/EmployeeEntity.java
+++ src/main/java/com/supermarket/backend/Entity/EmployeeEntity.java
@@ -1,5 +1,7 @@
 package com.supermarket.backend.Entity;
 
+import com.supermarket.backend.Enum.ERole;
+import com.supermarket.backend.Payload.Request.SignupRequest;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +26,9 @@ public class EmployeeEntity {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "email")
+    private String email;
+
     @Column(name = "full_name")
     private String fullName;
 
@@ -31,7 +36,7 @@ public class EmployeeEntity {
     private Date birthday;
 
     @Column(name = "role")
-    private String role;
+    private ERole role;
 
     @ManyToOne
     @JoinColumn(name = "department_id", referencedColumnName = "id")
@@ -39,4 +44,26 @@ public class EmployeeEntity {
 
     @Column(name = "status")
     private boolean status;
+
+    public EmployeeEntity(String username, String password, String email, String fullName, Date birthday, ERole role, DepartmentEntity department, boolean status) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+        this.birthday = birthday;
+        this.role = role;
+        this.department = department;
+        this.status = status;
+    }
+
+    public EmployeeEntity(SignupRequest signupRequest){
+        this.username = signupRequest.getUsername();
+        this.password = signupRequest.getPassword();
+        this.email = signupRequest.getEmail();
+        this.fullName = signupRequest.getFullName();
+        this.birthday = signupRequest.getBirthDay();
+        this.role = signupRequest.getRole();
+        this.department = signupRequest.getDepartment();
+        this.status = true;
+    }
 }
