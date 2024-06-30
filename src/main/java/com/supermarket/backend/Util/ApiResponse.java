ea4636a257b5eca8ac6395f5e11b0e7e96e1ffb9
Trần Minh Quang <tmquang0209@gmail.com>
2024-06-30 11:55:38 +0700

    chore: Refactor ApiResponse class to use Lombok annotations

--- src/main/java/com/supermarket/backend/Util/ApiResponse.java
+++ src/main/java/com/supermarket/backend/Util/ApiResponse.java
@@ -1,2 +1,18 @@
-package com.supermarket.backend.Util;public class ApiResponse {
+package com.supermarket.backend.Util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ApiResponse<T> {
+    private boolean success;
+    private List<T> data;
+    private String message;
 }
