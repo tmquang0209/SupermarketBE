5dadc71c87e518a13eea7ab51d7f526f2bd5929f
Trần Minh Quang <tmquang0209@gmail.com>
2024-07-12 19:19:32 +0700

    feat: Add BillRequest class for handling bill data in requests

---
+++ src/main/java/com/supermarket/backend/Payload/Request/BillRequest.java
@@ -0,0 +1,31 @@
+package com.supermarket.backend.Payload.Request;
+
+import com.supermarket.backend.Entity.CustomerEntity;
+import com.supermarket.backend.Entity.EmployeeEntity;
+import com.supermarket.backend.Enum.EPayment;
+import com.supermarket.backend.Payload.Dto.CartDTO;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BillRequest {
+    private EmployeeEntity employee;
+
+    @NotNull(message = "Customer id is required")
+    private CustomerEntity customer;
+
+    private String type;
+
+    private float discount;
+
+    private String promotionCode;
+
+    private int point = 0;
+
+    private EPayment paymentMethod = EPayment.CARD;
+
+    @NotNull(message = "Please add least 1 item to cart.")
+    private List<CartDTO> cart;
+}
